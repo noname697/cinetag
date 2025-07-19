cf74aea6ed69cf0251cb58d2f367d2c638c58e6e
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-19 19:56:46 -0300

    Componente de titulo

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,16 @@
 import Rodape from "components/Rodape";
 import Cabecalho from "components/Cabecalho";
 import Banner from "components/Banner";
+import Titulo from "components/Titulo";
 
 const Inicio = () => {
   return (
     <>
       <Cabecalho />
-      <Banner imagem="home"/>
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
       <Rodape />
     </>
   );

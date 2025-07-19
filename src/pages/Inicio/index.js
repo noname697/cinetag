c46ff687df2a86204bb5a1b364ca0541def65e19
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-19 20:06:52 -0300

    Componente de card

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,6 +2,7 @@ import Rodape from "components/Rodape";
 import Cabecalho from "components/Cabecalho";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
+import Card from "components/Card";
 
 const Inicio = () => {
   return (
@@ -11,6 +12,11 @@ const Inicio = () => {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card
+        id="1"
+        titulo="Gato bonifacio"
+        capa="https://thecatapi.com/api/images/get?format=src&type=png"
+      />
       <Rodape />
     </>
   );

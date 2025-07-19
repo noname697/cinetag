e75b0a876692ccea60352a3339dbe3857407ad8f
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-19 19:52:08 -0300

    Banner finalizado

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,12 @@
 import Rodape from "components/Rodape";
 import Cabecalho from "components/Cabecalho";
+import Banner from "components/Banner";
 
 const Inicio = () => {
   return (
     <>
       <Cabecalho />
+      <Banner imagem="home"/>
       <Rodape />
     </>
   );

8cbd0ec47a61e7675a634c9ac567a4b2ceee2278
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-25 13:26:03 -0300

    Banner

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,7 +1,24 @@
+import Banner from "components/Banner";
 import styles from "./Favoritos.module.css";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
 
 const Favoritos = () => {
-  return <>oi</>;
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <Card
+          id="1"
+          titulo="Gato bonifácio"
+          capa="https://thecatapi.com/api/images/get?format=src&type=png"
+        />
+      </section>
+    </>
+  );
 };
 
 export default Favoritos;

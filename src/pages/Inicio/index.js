f8da263a520b428774d98d85e11b4f5054251127
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-27 15:02:54 -0300

    API com json server

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,19 @@
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
-import videos from "json/db.json";
 import styles from "./Inicio.module.css";
+import { useEffect, useState } from "react";
 
 const Inicio = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/noname697/cinetag-api/videos")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(dados);
+      });
+  }, []);
   return (
     <>
       <Banner imagem="home" />
